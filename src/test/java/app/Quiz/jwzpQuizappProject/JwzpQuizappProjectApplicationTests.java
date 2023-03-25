abf9504a5a480b80336cae1b7ddd19a3297f6e95
Pladque <platekmichal791@gmail.com>
2023-03-25 11:36:02 +0100

    init app commit

---
+++ src/test/java/app/Quiz/jwzpQuizappProject/JwzpQuizappProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package app.Quiz.jwzpQuizappProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwzpQuizappProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
