15cb330a5aa441b55c7b3a53987cc63891c6728b
Tomasz Miśkowicz <tomasz.jan.miskowicz@gmail.com>
2023-04-21 14:09:45 +0200

    docker-compose with db, jwt auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-configuration-processor")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- src/main/java/app/Quiz/jwzpQuizappProject/JwzpQuizappProjectApplication.java
+++ src/main/java/app/Quiz/jwzpQuizappProject/JwzpQuizappProjectApplication.java
@@ -1,9 +1,12 @@
 package app.Quiz.jwzpQuizappProject;
 
+import app.Quiz.jwzpQuizappProject.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JwzpQuizappProjectApplication {
 
 	public static void main(String[] args) {
