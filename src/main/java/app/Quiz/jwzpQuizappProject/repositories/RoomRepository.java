5466ad12ebad919c30a5d37f3ae9aa950e3d5804
Tomasz Miśkowicz <tomasz.jan.miskowicz@gmail.com>
2023-06-05 17:59:21 +0200

    add new method to RoomRepository

--- src/main/java/app/Quiz/jwzpQuizappProject/repositories/RoomRepository.java
+++ src/main/java/app/Quiz/jwzpQuizappProject/repositories/RoomRepository.java
@@ -1,9 +1,13 @@
 package app.Quiz.jwzpQuizappProject.repositories;
 
 import app.Quiz.jwzpQuizappProject.models.rooms.RoomModel;
+import app.Quiz.jwzpQuizappProject.models.users.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends JpaRepository<RoomModel, Long> {
+    List<RoomModel> findAllByOwner(UserModel user);
 }
