8a2efc91675af94a253e0c095998c77dfa423ea3
Pladque <platekmichal791@gmail.com>
2023-06-12 14:53:39 +0200

    spring boot test some args

--- src/test/java/app/Quiz/jwzpQuizappProject/JwzpQuizappProjectApplicationTests.java
+++ src/test/java/app/Quiz/jwzpQuizappProject/JwzpQuizappProjectApplicationTests.java
@@ -4,15 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.Assert;
 
 //@RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest(classes = JwzpQuizappProjectApplicationTests.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 class JwzpQuizappProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
-//		Assert.isTrue(true, "test assertion");
+		Assert.isTrue(true, "test assertion");
 	}
 
 }
