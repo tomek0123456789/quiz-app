c7b1bc5ddb0440909724a4fd2607eac29b00ffcd
tomek <tomasz.jan.miskowicz@gmail.com>
2023-06-15 22:24:04 +0200

    email annotation validation

--- src/main/java/app/Quiz/jwzpQuizappProject/models/auth/RegisterDto.java
+++ src/main/java/app/Quiz/jwzpQuizappProject/models/auth/RegisterDto.java
@@ -7,7 +7,7 @@ public record RegisterDto(
         @NotBlank
         String name,
         @NotBlank
-        @Email
+        @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
         String email,
         @NotBlank
         String password
